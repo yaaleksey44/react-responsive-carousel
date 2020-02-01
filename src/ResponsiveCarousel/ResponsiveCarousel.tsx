cda03481dbbb041d4d2297b43772b5b89af7eeea
unknown <yaaleksey44@gmail.com>
2020-02-01 18:23:04 +0200

    first test commit

---
+++ src/ResponsiveCarousel/ResponsiveCarousel.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import styles from "./ResponsiveCarousel.module.css"
+
+
+
+
+
+const ResponsiveCarousel = () => {
+    return (
+        <div className={styles.container}>
+            ccc
+        </div>
+    )
+} 
